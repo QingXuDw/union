35fad81b4f18fd8fabfca0c519fab02b143a00fd
qingxu <qingxu@ubuntu.localdomain>
2020-10-21 22:50:26 -0700

    实现登入组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import login from '../components/login.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    component: login
+  }
 ]
 
 const router = new VueRouter({
